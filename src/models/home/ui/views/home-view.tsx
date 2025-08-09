740f5c2e3651e2dfcdacf2dcb392580d80475720
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-08-09 10:21:15 -0400

    latest commit

--- src/models/home/ui/views/home-view.tsx
+++ src/models/home/ui/views/home-view.tsx
@@ -1,14 +1,10 @@
 "use client";
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 export const HomeView = () => {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.hello.queryOptions({ text: "Utsav" }));
   return (
     <>
-      <div className="flex flex-col p-4 gap-y-4">{data?.greeting}</div>
+      <div className="flex flex-col p-4 gap-y-4">Home View</div>
     </>
   );
 };
