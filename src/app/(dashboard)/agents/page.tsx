d2d1af9f4982d733f8c0bab7e098798381176c24
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-08-10 10:08:38 -0400

    latest commit

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,9 +1,25 @@
-import { AgentsView } from "@/models/agents/ui/views/agents-view";
+import {
+  AgentsView,
+  AgentsViewLoading,
+  ErrorViewLoading,
+} from "@/models/agents/ui/views/agents-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-const Page = () => {
+const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
   return (
     <>
-      <AgentsView />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<AgentsViewLoading />}>
+          <ErrorBoundary fallback={<ErrorViewLoading />}>
+            <AgentsView />
+          </ErrorBoundary>
+        </Suspense>
+      </HydrationBoundary>
     </>
   );
 };
